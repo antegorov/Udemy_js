6a59851f39ce0554eaa86a9248bdaabdcb0c6b7b
Anton Egorov <antegorov@mail.ru>
2022-09-03 16:26:35 +0500

    first commit

---
+++ 004-006_lesson/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let number = 4.3;
+console.log('string' * 9);
+
+const person = `5`;
+
+const bool = false;
+
+
+console.log(something);
+
+let und;
+console.log(und);
+
+
+//object
+// колелкция любых данных (комплексный тип)
+
+const obj = {
+  name: 'John',
+  age: '25',
+  isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[2]);
+
---
+++ 007-008_lesson/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(answer + 5);
+
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Какова ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+
+
+
+// lesson_8
+
+/* // До интерполяции была конкатенация строк:
+const category = 'toys';
+console.log('https://someurl.com/' + category + '/' + '4'); */
+
+
+
+/* // Потом появилось интерполяция
+const category = 'toys';
+console.log(`https://someurl.com/${category}/4`);
+
+
+const user = 'Iven';
+alert(`Hello, ${user}`); */
